fed6dd7cd1a797fb24c6e07a4d2e456b53795fd3
1779518 <suggestedmailname@gmail.com>
2020-11-25 04:26:08 -0500

    modification de la page de cheminement (ajout des séparateurs de section) et changement du css

---
+++ pages/grilleCheminement.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="../css/cheminement.css">
+  <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+
+<div id="background-shapes">
+    <div class="background-shape color-bg-2" id="background-shape-left"></div>
+    <div class="background-shape color-bg-2" id="background-shape-right"></div>
+</div>
+
+<div class="animation-area">
+    <ul class="box-area">
+        <li>+</li>
+        <li>+</li>
+        <li>+</li>
+        <li>+</li>
+        <li>+</li>
+        <li>+</li>
+        <li>+</li>
+        <li>+</li>
+
+    </ul>
+</div>
+
+<div id="contenu-principal">
+  <h1>⠀</h1>
+
+  <div class="grid-container">
+
+    <!-- Cours web, cours prérequis, cours video, cours design-->
+    <div class="grid-item-legende">
+      <div class="grid-item chem-cell-top-legend"><div class="chem-cell">
+        <ul class="chem-legende">
+          <li class=""><div class="chem-legend-circle color-1">COURS</div></li>
+          <li class="chem-legend-cell"><div class="chem-legend-circle color-2">COURS PROGRAMMATION</div></li>
+          <li class="chem-legend-cell"><div class="chem-legend-circle color-3">COURS DESIGN</div></li>
+          <li class="chem-legend-cell"><div class="chem-legend-circle color-5">COURS PRÉREQUIS</div></li>
+        </ul>
+      </div></div>
+    </div>
+
+    <div class="grid-item chem-cell-separator chem-cell-2 color-bars"></div>
+    <div class="grid-item chem-cell-top color-text-1 chem-session-title">SESSION 1</div>
+    <div class="grid-item chem-cell-separator chem-cell-4 color-bars"></div>
+    <div class="grid-item chem-cell-top color-text-1 chem-session-title">SESSION 2</div>
+    <div class="grid-item chem-cell-separator chem-cell-6 color-bars"></div>
+    <div class="grid-item chem-cell-top color-text-1 chem-session-title">SESSION 3</div>
+    <div class="grid-item chem-cell-separator chem-cell-8 color-bars"></div>
+    <div class="grid-item chem-cell-top color-text-1 chem-session-title">SESSION 4</div>
+    <div class="grid-item chem-cell-separator chem-cell-10 color-bars"></div>
+    <div class="grid-item chem-cell-top color-text-1 chem-session-title">SESSION 5</div>
+    <div class="grid-item chem-cell-separator chem-cell-12 color-bars"></div>
+    <div class="grid-item chem-cell-top color-text-1 chem-session-title">SESSION 6</div>
+    <div class="grid-item chem-cell-separator chem-cell-14"></div>
+
+
+    <!-- Rangée 1-->
+    <div class="grid-item"></div>
+    <div class="grid-item-stage chem-cell-top color-1">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-1" ></div><a class="chem-lien" href="profileCours.html">Stages</a></div></div>
+    <div class="grid-item chem-cell-top color-1">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-1" ></div><a class="chem-lien" href="profileCours.html">Animation 3D</a></div></div>
+    <div class="grid-item chem-cell-top color-1">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-1" ></div><a class="chem-lien" href="profileCours.html">Animation 3D</a></div></div>
+    <div class="grid-item chem-cell-top color-1">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-1" ></div><a class="chem-lien" href="profileCours.html">Animation 3D</a></div></div>
+    <div class="grid-item chem-cell-top color-1">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-1" ></div><a class="chem-lien" href="profileCours.html">Animation 3D</a></div></div>
+    <div class="grid-item chem-cell-top color-1">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-1" ></div><a class="chem-lien" href="profileCours.html">Animation 3D</a></div></div>
+
+    <!-- Rangée 2-->
+    <div class="grid-item"></div>
+    <div class="grid-item chem-cell-top color-1">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-1" ></div><a class="chem-lien" href="profileCours.html">Technologie émergentes et avancées: volet création</a> </div>  </div>
+    <div class="grid-item chem-cell-top color-1">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-1" ></div><a class="chem-lien" href="profileCours.html">Technologie émergentes et avancées: volet prog.</a></div></div>
+    <div class="grid-item chem-cell-top color-1">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-1" ></div><a class="chem-lien" href="profileCours.html">Création vidéo</a></div></div>
+    <div class="grid-item chem-cell-top color-1">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-1" ></div><a class="chem-lien" href="profileCours.html">Effets spéciaux et animation</a></div></div>
+    <div class="grid-item chem-cell-top color-1">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-1" ></div><a class="chem-lien" href="profileCours.html">Imagerie 3D</a></div></div>
+    
+    <!-- Rangée 3-->
+    <div class="grid-item"></div>  
+    <div class="grid-item-projet chem-cell-top color-2">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-2" ></div><a class="chem-lien" href="profileCours.html"> Projet de fin d'étude</a></div></div> 
+    <div class="grid-item chem-cell-top color-1">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-1" ></div><a class="chem-lien" href="profileCours.html">Méthode de recherche et de préparation au marché du travail</a></div></div>
+    <div class="grid-item chem-cell-top color-1">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-1" ></div><a class="chem-lien" href="profileCours.html">Conception graphique et img. matricielle</a></div></div> 
+    <div class="grid-item chem-cell-top color-1">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-1" ></div><a class="chem-lien" href="profileCours.html">Conception graphique et img. vectorielle</a></div></div>
+    <div class="grid-item chem-cell-top color-2">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-2" ></div><a class="chem-lien" href="profileCours.html">Design d'intéractivité</a></div></div>
+    <div class="grid-item chem-cell-top color-2">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-2" ></div><a class="chem-lien" href="profileCours.html">Gestion de projet multimedia</a></div></div> 
+    <!-- Rangée 4-->
+    <div class="grid-item"></div>
+    <div class="grid-item chem-cell-top color-2">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-2" ></div><a class="chem-lien" href="profileCours.html">Mise en page web</a></div></div> 
+    <div class="grid-item chem-cell-top color-2">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-2" ></div><a class="chem-lien" href="profileCours.html">Animation et interactivité web</a></div></div>
+    <div class="grid-item chem-cell-top color-2">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-2" ></div><a class="chem-lien" href="profileCours.html">Céation de site web dynamique</a></div></div>
+    <div class="grid-item chem-cell-top color-3">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-3" ></div><a class="chem-lien" href="profileCours.html">Conception d'interfaces et développement web</a></div></div>
+    <div class="grid-item chem-cell-top color-3">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-3" ></div><a class="chem-lien" href="profileCours.html">Projet web en équipe</a></div></div>
+    <!-- Rangée 5-->
+    <div class="grid-item"></div>  
+    <div class="grid-item chem-cell-top color-3">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-3" ></div><a class="chem-lien" href="profileCours.html">Création de jeu 2D</a></div></div>
+    <div class="grid-item chem-cell-top color-3">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-3" ></div><a class="chem-lien" href="profileCours.html">Création de jeu 3D</a></div></div>
+    <div class="grid-item chem-cell-top color-3">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-3" ></div><a class="chem-lien" href="profileCours.html">Création de jeu en équipe</a></div></div>  
+    <div class="grid-item chem-cell-top color-3">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-3" ></div><a class="chem-lien" href="profileCours.html">expérimentation en jeu: volet création</a></div></div>
+    <div class="grid-item chem-cell-top color-3">0000-0000<div class="color-bars chem-cell"><div class = "chem-cell-header color-3" ></div><a class="chem-lien" href="profileCours.html">expérimentation en jeu: volet prog.</a></div></div>
+    
+  </div>
+</div>
+
+
+
+</body>
+<footer class="footer color-bars">
+  <div class="credits" >
+      <p>Site Web crée par:</p>
+      <p>Gabriel Alvarez, Louis Horlaville, Thomas Lorenzo, Kevin Ponce</p>
+  </div>
+
+  <div class="contacts" >
+      <p>3 800, rue Sherbrooke Est Montréal (Québec) H1X 2A2 | 514 254-7131</p>
+      <p>Copyright © 2020 - Collège de Maisonneuve. Tous droits réservés.</p>
+  </div>
+</footer>
+
+</html>
