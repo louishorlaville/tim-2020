e14e21797ea56404c272d4f6fd07909621c5ef16
kevinPM <kevinmelendez16@hotmail.com>
2020-11-27 03:51:16 -0500

    Page evenement : Placement des cases du stade -KPM

--- pages/pageEvenements.php
+++ pages/pageEvenements.php
@@ -7,19 +7,19 @@
 	<!-- Fin Header -->
 	<h1 id="titrePage">La vie étudiante au TIM</h1>
 	<main>
-		<section id="stade">
-		<img src="../images/svg/stade/stadeBase.svg" alt="" id="baseStade">
-		<img src="../images/svg/stade/stadeCase1.svg" alt="" class="casesStade">
-		<img src="../images/svg/stade/stadeCase2.svg" alt="" class="casesStade">
-		<img src="../images/svg/stade/stadeCase3.svg" alt="" class="casesStade">
-		<img src="../images/svg/stade/stadeCase4.svg" alt="" class="casesStade">
-		<img src="../images/svg/stade/stadeCase5.svg" alt="" class="casesStade">
-		<img src="../images/svg/stade/stadeCase6.svg" alt="" class="casesStade">	    
+		<section id="stadeContainer">
+			<img src="../images/svg/stade/stadeBase.svg" alt="" id="baseStade">
+			<img src="../images/svg/stade/stadeCase1.svg" alt="" class="casesStade">
+			<img src="../images/svg/stade/stadeCase2.svg" alt="" class="casesStade">
+			<img src="../images/svg/stade/stadeCase3.svg" alt="" class="casesStade">
+			<img src="../images/svg/stade/stadeCase4.svg" alt="" class="casesStade">
+			<img src="../images/svg/stade/stadeCase5.svg" alt="" class="casesStade">
+			<img src="../images/svg/stade/stadeCase6.svg" alt="" class="casesStade">	    
 		</section>
 		<section id="infoEvenement">
 			<h1 id="titreEvenenment">Nom de l'événement</h1>
-			<h5 id="dateEvenenement">04/11/2019</h5>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium voluptatum, rem! Natus aperiam, molestias doloribus repellendus eos nostrum sed neque nobis iusto expedita consectetur esse, optio assumenda, provident quidem ex?</p>
+			<h5 id="dateEvenement">04/11/2019</h5>
+			<p>lorem</p>
 			<div id="carousel">
 				<span>&lt;</span>
 				<div id="cadreImage">
