017f9ec310f70c77f4468661310d938777e17629
Louis Horlaville <louis.horlaville@gmail.com>
2020-11-27 19:28:59 -0500

    ajusté le display flex du footer en format bureau et tablette

--- inclusions/piedDePage.php
+++ inclusions/piedDePage.php
@@ -1,12 +1,15 @@
 <footer class="footer color-bars">
     <div class="credits" >
         <p>Site Web crée par:</p>
-        <p>Gabriel Alvarez, Louis Horlaville, Thomas Lorenzo, Kevin Ponce</p>
+        <p>Gabriel Alvarez, Louis Horlaville</p>
+        <p>Thomas Lorenzo, Kevin Ponce</p>
     </div>
-
+    <div class="college">
+        <p><a id="liens-college-footer" href="https://www.cmaisonneuve.qc.ca/" target="_blank" >Collège de Maisonneuve.</a></p>
+    </div>
     <div class="contacts" >
         <p>3 800, rue Sherbrooke Est Montréal (Québec)</p><p> H1X 2A2 | 514 254-7131 - Copyright © 2020 -</p>
-        <p>Techniques d'Intégration Mutlimédia,</p><p><a id="liens-college-footer" href="https://www.cmaisonneuve.qc.ca/" target="_blank" >Collège de Maisonneuve.</a>  Tous droits réservés.</p>
+        <p>Techniques d'Intégration Mutlimédia,</p><p>  Tous droits réservés.</p>
     </div>
 </footer>
 </body>
