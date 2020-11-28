6fb118aa7504a9573150cec33ae014f97086e0f9
Louis Horlaville <louis.horlaville@gmail.com>
2020-11-28 00:51:55 -0500

    ajusté le footer pour la version mobile et bureau avec le logo qui apparait seulement sur les pages qui ne sont pas l'accueil

--- inclusions/piedDePage.php
+++ inclusions/piedDePage.php
@@ -1,15 +1,20 @@
 <footer class="footer color-bars">
     <div class="credits" >
-        <p>Site Web crée par:</p>
+        <p>Site Web créé par:</p>
         <p>Gabriel Alvarez, Louis Horlaville</p>
         <p>Thomas Lorenzo, Kevin Ponce</p>
     </div>
     <div class="college">
-        <p><a id="liens-college-footer" href="https://www.cmaisonneuve.qc.ca/" target="_blank" >Collège de Maisonneuve.</a></p>
+        <a id="liens-college-footer" href="https://www.cmaisonneuve.qc.ca/" target="_blank" > 
+            <?php if($page!="accueil"): ?>
+                <img src="./images/svg/logoTim.svg" alt="Logo TIM">
+            <?php endif; ?>
+            <p>Collège de Maisonneuve</p>
+        </a>
     </div>
     <div class="contacts" >
         <p>3 800, rue Sherbrooke Est Montréal (Québec)</p><p> H1X 2A2 | 514 254-7131 - Copyright © 2020 -</p>
-        <p>Techniques d'Intégration Mutlimédia,</p><p>  Tous droits réservés.</p>
+        <p>Techniques d'Intégration Multimédia,</p><p>  Tous droits réservés.</p>
     </div>
 </footer>
 </body>
