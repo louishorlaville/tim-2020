d93198b2188f20bca0f3381e61d2647173bb38f7
kevinPM <kevinmelendez16@hotmail.com>
2020-12-09 03:29:59 -0500

    Page evenement : Positionement du stade continuation -KPM

--- pages/evenements.php
+++ pages/evenements.php
@@ -11,23 +11,24 @@
 			<div id="casesConteneur">
 				<img src="../images/svg/stade/stadeBase.svg" alt="" id="baseStade">
 				<img src="../images/svg/stade/stadeCase1.svg" alt="" class="casesStade">
-				<!-- <img src="../images/svg/stade/stadeCase2.svg" alt="" class="casesStade">
+				<img src="../images/svg/stade/stadeCase2.svg" alt="" class="casesStade">
 				<img src="../images/svg/stade/stadeCase3.svg" alt="" class="casesStade">
 				<img src="../images/svg/stade/stadeCase4.svg" alt="" class="casesStade">
 				<img src="../images/svg/stade/stadeCase5.svg" alt="" class="casesStade">
-				<img src="../images/svg/stade/stadeCase6.svg" alt="" class="casesStade"> -->
+				<img src="../images/svg/stade/stadeCase6.svg" alt="" class="casesStade">
 			</div>		    
 		</section>
 		<section id="infoEvenement">
 			<h1 id="titreEvenenment">Nom de l'événement</h1>
 			<h5 id="dateEvenement">04/11/2019</h5>
-			<p>lorem</p>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, at officiis.
+				 Fugiat officia quas aliquid tempora iste eaque quibusdam cupiditate? 
+				 Lorem ipsum dolor sit amet.</p>
 			<div id="carousel">
 				<span>&lt;</span>
 				<div id="cadreImage">
 					<img src="../images/img/profTemp.jpg" alt="Photo pas disponible">
 				</div>
-				
 				<span>&gt;</span>
 			</div>
 		</section>
