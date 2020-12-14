52f32f91be6663c8f3ab5a2c118219f1d8ed0c09
kevinPM <kevinmelendez16@hotmail.com>
2020-12-13 20:45:56 -0500

    Page evenement : JS -KPM

--- js/evenement.js
+++ js/evenement.js
@@ -1,9 +1,3 @@
-for (let index = 0; index < 5; index++) {
-    document.getElementsByClassName("casesStade")[index].addEventListener("click", function () {
-        document.getElementById("infoEvenement").style.display = "block"
-    });   
-}
-
 let evenementData;
 getClassesJSON();
 
@@ -12,9 +6,9 @@ function getClassesJSON() {
   let xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      profsData = JSON.parse(this.responseText);
-      console.log(EvenementsData);
-      console.log("loaded profs data");
+      evenementData = JSON.parse(this.responseText);
+      console.log(evenementData);
+      console.log("loaded evenement data");
     }
   };
   xmlhttp.open("GET", "../data/evenements.json", true);
@@ -25,4 +19,15 @@ let BoiteInfos = document.querySelector("#infoEvenement");
 let titre = document.querySelector("#titreEvenement");
 let date = document.querySelector("#dateEvenement");
 let desc = document.querySelector("#descEvenement");
-let image = document.querySelector("#imageEvenement")
+let image = document.querySelector("#imageEvenement");
+
+for (let index = 0; index < 5; index++) {
+    document.getElementsByClassName("casesStade")[index].addEventListener("click", function () {
+        document.getElementById("infoEvenement").style.display = "block";
+
+        titre.innerHTML = evenementData.evenements[index].titre.toUpperCase();
+        date.innerHTML = evenementData.evenements[index].date.toUpperCase();
+        desc.innerHTML = evenementData.evenements[index].description.toUpperCase();
+        image.src = "../images/img/evenements/" + evenementData.evenements[index].photo; 
+    });   
+}
